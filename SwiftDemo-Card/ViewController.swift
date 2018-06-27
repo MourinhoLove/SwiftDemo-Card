b0e1b723457d63fdb4301d0f1f5fbe8a9980262d
Franklin-ZQ <1318577179@qq.com>
2018-06-27 17:34:39 +0800

    First submit

--- SwiftDemo-Card/ViewController.swift
+++ SwiftDemo-Card/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        print("First");
         // Do any additional setup after loading the view, typically from a nib.
     }
 
